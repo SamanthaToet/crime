22c7926adf8f8e9aa5c1b5eb37ee3a4e6540bed0
Samantha Toet <samanthactoet@gmail.com>
2024-03-19 11:36:56 -0400

    building persistent odp api

--- cville_data.R
+++ cville_data.R
@@ -248,17 +248,41 @@ table(arrests$statute_description) %>% sort()
 
 # Live gun API ----
 
+# There are 3 incident types that directly pertain to guns: "Shots Fired/Illegal Hunting", "Robbery - Armed", "Weapons Violations"
+
 gun_where_clause <- "%28Offense%20LIKE%20'%shot%'%29OR%28Offense%20LIKE%20'%armed%'%29OR%28Offense%20LIKE%20'%weapon%'%29"
 gun_api <- glue::glue("https://gisweb.charlottesville.org/arcgis/rest/services/OpenData_2/MapServer/6/query?where={gun_where_clause}&outFields=*&outSR=4326&f=json")
 gun_response <- fromJSON(gun_api)
 
-gun_data <- gun_response$features$attributes%>% #512 obs
+gun_data <- gun_response$features$attributes%>% #~500 obs
   janitor::clean_names() %>%
   mutate(block_number = ifelse(block_number == "", NA, block_number)) %>%
   mutate(date_reported = date_reported %>% gsub('000$', '', .) %>% as.numeric() %>% as.POSIXct())
 
+#TODO - mechanism for missing block numbers (17%) which are primary intersections 
 
-#TODO - mechanism for missing block numbers (17%)
+gun_data %<>% mutate(address = paste(block_number, street_name, "Charlottesville VA"))
+lon_lat <- geocode(gun_data$address) #takes ~10 minutes
+gun_calls <- bind_cols(gun_data, lon_lat)
+
+#TODO - schedule this report to run (daily? weekly?)
+
+ggmap(cville_map) +
+  stat_density2d(data = gun_calls, aes(fill = ..level.., alpha = 0.1),
+                 geom = "polygon") +
+  theme(legend.position="none") +
+  scale_fill_viridis_c(direction = -1) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  ggtitle("Gun-Related Calls Available in the Open Data Portal")
+
+
+
+
 
 
 
